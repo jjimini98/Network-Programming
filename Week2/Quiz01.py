2dd87ef2805de90ac3eacd21efda64567d411ce1
jimin <jjimini98@naver.com>
2021-03-10 11:43:07 +0900

    proceeding Week2 Quiz01

---
+++ Week2/Quiz01.py
@@ -0,0 +1,15 @@
+name = "My name is jimin Yoo "
+
+
+print(name)
+
+print("length of String " ,len(name))
+print(name*10)
+print(name[0])
+print(name[:4])
+print(name[-4:])
+print(name[::1])
+print()
+print(upper(name))
+print(lower(name))
+print(name.replace('a','e'))
