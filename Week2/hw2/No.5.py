88cec7ba1b6048847fb805053b1c48173b93a6e4
jimin <jjimini98@naver.com>
2021-03-12 19:13:28 +0900

    finished No.5.py

---
+++ Week2/hw2/No.5.py
@@ -0,0 +1,12 @@
+lis = []
+for x in range(0,50):
+    lis.append(x)
+
+print("0~49까지의 수로 구성된 리스트는" ,lis)
+
+
+lis2=[]
+for x in range(0,50):
+    lis2.append(x**2)
+
+print("0~49까지의 수의 제곱으로 구성된 리스트는", lis2)
