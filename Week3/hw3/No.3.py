96bc9f3d5ebc0f863b8d38b6a4fc9a70b1c94ccd
jimin <jjimini98@naver.com>
2021-03-22 14:47:49 +0900

    finished No.3.py

--- Week3/hw3/No.3.py
+++ Week3/hw3/No.3.py
@@ -1,10 +1,16 @@
 #3. 다음과 같이 구성되는 문자열을 구분 문자로 분리해 딕셔너리로 반환하는 함수를 포함하는 프로그램 작성
-st = []
 def splitor(string):
-    string = string.split("&")
-    for x in string:
-        x= dict.fromkeys().split("=")
+    di = {}
+    st = []
 
-    return  x
+    string = string.split("&")
+
+    for x in string:
+      st.append(x.split("="))
+
+    for t in st:
+        di[t[0]] = t[1]
+
+    return  di
 
 print(splitor("led=on&motor=off&switch=off"))
