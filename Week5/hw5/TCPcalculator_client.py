6ff1c3e64d8890cf1e3a9fea393196064f0711be
jimin <jjimini98@naver.com>
2021-04-02 15:46:21 +0900

    finished TCPcalculator_client.py

---
+++ Week5/hw5/TCPcalculator_client.py
@@ -0,0 +1,19 @@
+from socket import *
+
+#클라이언트 소켓 생성 및 서버의 소켓과 연결
+s = socket(AF_INET , SOCK_STREAM)
+s.connect(('localhost', 3333))
+
+
+while True:
+   user_input = input("계산 식을 입력하세요: ")
+   if user_input == "q":
+      break 
+
+   # 서버로 계산식 전송   
+   s.send(user_input.encode())
+
+   #서버에서 계산된 값을 받기
+   print("Received message : ", s.recv(1024).decode())
+
+s.close()
