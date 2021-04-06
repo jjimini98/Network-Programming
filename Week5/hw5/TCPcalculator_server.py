059a578953a2bc00b95e8e8c023efe7207de513f
jimin <jjimini98@naver.com>
2021-04-06 15:43:03 +0900

    finished TCPcalculator_server.py

---
+++ Week5/hw5/TCPcalculator_server.py
@@ -0,0 +1,43 @@
+from socket import *
+
+
+def calculator(sentence):
+    sentence = sentence.split(" ")
+
+    if sentence[1] == "+":
+        return int(sentence[0]) + int(sentence[2])
+
+    elif sentence[1] == "-":
+        return int(sentence[0]) - int(sentence[2])
+
+    elif sentence[1] == "*":
+        return int(sentence[0]) * int(sentence[2])
+
+    elif sentence[1] == "/":
+        return '%0.1f' % float(float(sentence[0]) / float(sentence[2]))
+
+
+s = socket(AF_INET, SOCK_STREAM)
+s.bind(('', 3331))
+s.listen(5)
+
+print("waiting....")
+
+while True:
+    client, addr = s.accept()
+    print("Connection from : ", addr)
+
+    while True:
+        data = client.recv(1024)
+        if not data: break
+        try:
+            result = calculator(data.decode())
+        except:
+            client.send(b"Try Again")
+
+        else:
+            client.send(str(result).encode())
+
+    client.close()
+
+s.close()
