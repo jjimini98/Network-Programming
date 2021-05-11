457b6ec4c23607efed5219233f0adce10888963b
jimin <jjimini98@naver.com>
2021-05-11 17:28:09 +0900

    일단 서버에 둘 다 기록되는 것을 확인 + 이제 각 클라이언트에서 기록이 되어야함

--- Week10/hw11/select_multi_chat_server.py
+++ Week10/hw11/select_multi_chat_server.py
@@ -17,23 +17,24 @@ print(str(PORT) + " 에서 접속 대기중")
 while True: 
 
    r_sock , w_sock, e_sock = select.select(s_list, [], [])
-   print(s_list)
-   for s in s_list:
+   for s in r_sock:
+      print("r_sock is", r_sock)
+      print("s_sock is" , s_sock) 
       if s== s_sock:
          c_sock, addr = s_sock.accept()
+         print("c_sock is", c_sock)
          s_list.append(c_sock)
          print("Client {} connected".format(addr))
-         data = c_sock.recv(BUFFER)
-         print("Received : ", data.decode())
-         for s in s_list :
-            if s!=s_sock:
-               c_sock.send(data)
-      else: 
-         data = c_sock.recv(BUFFER)
-         if not data :
+   
+      elif s != s_sock:      
+         data = s.recv(BUFFER)
+         if data.decode() == "quit" :
             s.close()
             s_list.remove(s)
             continue
-
+         
+         print("send complete")
          print("Received : ", data.decode())
-      
+         if s != c_sock:
+            s.send(data)
+
