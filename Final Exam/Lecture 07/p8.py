8c2301cd3141a019f0a6afc67c73eb500ba4a262
jimin <jjimini98@naver.com>
2021-05-27 16:04:13 +0900

    p8 완료

--- Final Exam/Lecture 07/p8.py
+++ Final Exam/Lecture 07/p8.py
@@ -4,4 +4,17 @@ import threading
 def prtSquare(num):
    print("Square : {}".format(num**2))
 
-def prtCub 
+def prtCube(num):
+   print("Cube : {}".format(num**3))
+
+
+t1 = threading.Thread(target=prtSquare, args=(10,))
+t2 = threading.Thread(target=prtCube, args=(10,))
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
+
+print("Done!")
