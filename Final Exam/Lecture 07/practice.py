7459588045d4be34944a4b21cd83f68f114451b3
jimin <jjimini98@naver.com>
2021-05-27 15:55:47 +0900

    Lecture 07 1회독 시작

---
+++ Final Exam/Lecture 07/practice.py
@@ -0,0 +1,18 @@
+# p.3
+
+from socket import *
+
+s = socket(AF_INET , SOCK_STREAM)
+s.bind(('',9000))
+s.listen(2)
+
+
+while True: 
+   client, addr = s.accept()
+   print("Connected from" , addr)
+   print("addr is " , addr[0])
+   client.send(b"Hello "+ addr[0].encode())
+   client.close() 
+
+
+import threading 
